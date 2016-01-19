0fdcf7a74e3d385a6ce891fb44dc945e275f0cf1
jjee <rofflemywofflepl0x@gmail.com>
2016-01-19 10:46:42 -0500

    pls

--- src/dafaq/Main.java
+++ src/dafaq/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         double [][] test ={{85,90,80},{86,88,90},{84,92,89},{88,95,82}};
 
-        for (int column = 0; column < test[0].length; column++) {
+
+        for(int row=0;row<test[0].length;row++){
             double total=0;
-            for (int row = 0; row < test.length; row++) {
-                total+=test[row][column];
-                System.out.println(total);
-           }
 
         }
+
+
+
 
 
 
